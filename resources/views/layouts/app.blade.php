675a643e63221b3bc6e1e2014413af1c2c873a4e
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-03 19:18:05 +0330

    Add login screen with custom Livewire component and Flux UI integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,13 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @livewireStyles
+
+        @fluxAppearance
     </head>
     <body>
         {{ $slot }}
 
         @livewireScripts
+        @fluxScripts
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::livewire('/login', \App\Livewire\Front\Auth\Login::class)->name('login');
