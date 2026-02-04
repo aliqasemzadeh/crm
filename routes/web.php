b29be6a220f700e2997c891e3c6ff21c81fc38ea
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-04 07:57:58 +0330

    Add Vazirmatn font dependency and integrate it into Tailwind configuration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/login', \App\Livewire\Auth\Login::class)->name('login');
 Route::livewire('/register', \App\Livewire\Auth\Register::class)->name('register');
+Route::livewire('/forget-password', \App\Livewire\Auth\ForgetPassword::class)->name('forget-password');
+Route::livewire('/change-password/{token}', \App\Livewire\Auth\ChangePassword::class)->name('change-password');
 
 
 Route::middleware(['auth', 'verified'])->group( function () {
