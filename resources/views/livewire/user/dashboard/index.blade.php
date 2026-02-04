f0d7b01c81a5586eec17acf402913619f6151fc2
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-04 15:34:23 +0330

    Move dashboard layout elements to the application layout for better reusability and cleanup

--- resources/views/livewire/user/dashboard/index.blade.php
+++ resources/views/livewire/user/dashboard/index.blade.php
@@ -1,83 +1,5 @@
 <div>
-    <flux:header class="bg-zinc-50 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-700">
-        <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
-
-        <flux:brand href="#" logo="/img/demo/logo.png" name="Acme Inc." class="max-lg:hidden dark:hidden" />
-        <flux:brand href="#" logo="/img/demo/dark-mode-logo.png" name="Acme Inc." class="max-lg:hidden! hidden dark:flex" />
-
-        <flux:navbar class="max-lg:hidden">
-            <flux:dropdown>
-                <flux:navbar.item icon:trailing="chevron-down">Recent</flux:navbar.item>
-
-                <flux:navmenu>
-                    <flux:navbar.item href="#">Projects</flux:navbar.item>
-                    <flux:navbar.item href="#">Tasks</flux:navbar.item>
-                    <flux:navbar.item href="#">Files</flux:navbar.item>
-                </flux:navmenu>
-            </flux:dropdown>
-
-            <flux:dropdown>
-                <flux:navbar.item icon:trailing="chevron-down">Starred</flux:navbar.item>
-
-                <flux:navmenu>
-                    <flux:navbar.item href="#">Users</flux:navbar.item>
-                    <flux:navbar.item href="#">Events</flux:navbar.item>
-                    <flux:navbar.item href="#">Products</flux:navbar.item>
-                </flux:navmenu>
-            </flux:dropdown>
-        </flux:navbar>
-
-        <flux:spacer />
-
-        <flux:navbar class="mr-4">
-            <flux:navbar.item square icon="magnifying-glass" href="#" label="Search" />
-            <flux:navbar.item class="max-lg:hidden" square icon="cog-6-tooth" href="#" label="Settings" />
-            <flux:navbar.item class="max-lg:hidden" square icon="information-circle" href="#" label="Help" />
-        </flux:navbar>
-
-        <flux:profile avatar="/img/demo/user.png" />
-    </flux:header>
-
-    <flux:sidebar sticky collapsible="mobile" class="lg:hidden bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
-        <flux:sidebar.header>
-            <flux:sidebar.brand
-                href="#"
-                logo="https://fluxui.dev/img/demo/logo.png"
-                logo:dark="https://fluxui.dev/img/demo/dark-mode-logo.png"
-                name="Acme Inc."
-            />
-
-            <flux:sidebar.collapse class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
-        </flux:sidebar.header>
-
-        <flux:sidebar.nav>
-            <flux:sidebar.item icon="home" href="#" current>Home</flux:sidebar.item>
-            <flux:sidebar.item icon="inbox" badge="12" href="#">Inbox</flux:sidebar.item>
-            <flux:sidebar.item icon="document-text" href="#">Documents</flux:sidebar.item>
-            <flux:sidebar.item icon="calendar" href="#">Calendar</flux:sidebar.item>
-
-            <flux:sidebar.group expandable heading="Favorites" class="grid">
-                <flux:sidebar.item href="#">Marketing site</flux:sidebar.item>
-                <flux:sidebar.item href="#">Android app</flux:sidebar.item>
-                <flux:sidebar.item href="#">Brand guidelines</flux:sidebar.item>
-            </flux:sidebar.group>
-        </flux:sidebar.nav>
-
-        <flux:sidebar.spacer />
-
-        <flux:sidebar.nav>
-            <flux:sidebar.item icon="cog-6-tooth" href="#">Settings</flux:sidebar.item>
-            <flux:sidebar.item icon="information-circle" href="#">Help</flux:sidebar.item>
-        </flux:sidebar.nav>
-    </flux:sidebar>
-
-    <flux:main>
         <div class="flex flex-col md:flex-row gap-6 justify-between md:items-center mb-6">
-            <flux:breadcrumbs>
-                <flux:breadcrumbs.item href="#" divider="slash">Acme Inc.</flux:breadcrumbs.item>
-                <flux:breadcrumbs.item href="#" divider="slash">iOS App V2</flux:breadcrumbs.item>
-            </flux:breadcrumbs>
-
             <div class="flex gap-4">
                 <flux:dropdown position="bottom" align="end">
                     <flux:button size="sm" variant="filled" icon:trailing="chevron-down">Filters</flux:button>
@@ -140,5 +62,4 @@
                 @endforeach
             </div>
         </div>
-    </flux:main>
 </div>
