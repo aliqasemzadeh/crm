2f4e118ff096dbb88859425d676754f48aea7bdf
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-04 06:15:33 +0330

    Add CRM dashboard Livewire component and route

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/login', \App\Livewire\Front\Auth\Login::class)->name('login');
+
+
+Route::livewire('/crm/dashboard/index', \App\Livewire\Crm\Dashboard\Index::class)->name('crm.dashboard.index');
