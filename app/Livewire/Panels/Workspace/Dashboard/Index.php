c4c99f07843094b4e998046b4a08cf2407b2e7d3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-06 07:39:36 +0330

    Add Livewire `Index` component and view for workspace dashboard panel

---
+++ app/Livewire/Panels/Workspace/Dashboard/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Dashboard;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.dashboard.index');
+    }
+}
