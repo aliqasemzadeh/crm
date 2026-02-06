1ee12d4e707d9807a999800602f290719dacac7f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-06 15:39:29 +0330

    Move `ChangeMobile` component and view under `User/Setting` namespace and directory

--- app/Livewire/Panels/Administrator/UserManagement/User/Create.php
+++ app/Livewire/Panels/Administrator/UserManagement/User/Create.php
@@ -10,15 +10,31 @@ class Create extends Component
 {
     public string $mobile = '';
 
+    public string $first_name = '';
+
+    public string $last_name = '';
+
+    public string $email = '';
+
+    public string $password = '';
+
+    public string $password_confirmation = '';
+
     public function create()
     {
         $this->authorize('administrator_user_management_create');
 
         $validated = $this->validate([
-            'mobile' => ['required', 'string', 'lowercase', 'ir_mobile', 'max:255', 'unique:'.User::class],
+            'first_name' => ['nullable', 'string', 'max:255'],
+            'last_name' => ['nullable', 'string', 'max:255'],
+            'mobile' => ['required', 'string', 'ir_mobile', 'max:255', 'unique:'.User::class],
+            'email' => ['nullable', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', \Illuminate\Validation\Rules\Password::defaults()],
         ]);
 
-        User::firstOrCreate($validated);
+        User::create($validated);
+
+        $this->reset(['mobile', 'first_name', 'last_name', 'email', 'password', 'password_confirmation']);
 
         $this->dispatch('panels.administrator.user-management.user.index.render');
         Flux::modal('panels.administrator.user-management.user.create.modal')->close();
