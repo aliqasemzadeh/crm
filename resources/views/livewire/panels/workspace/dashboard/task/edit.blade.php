88a5395f0ce620b50c71486b6e27063d8c02c1e8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-07 18:49:52 +0330

    Update Persian translations for tasks and fix modal text consistency

--- resources/views/livewire/panels/workspace/dashboard/task/edit.blade.php
+++ resources/views/livewire/panels/workspace/dashboard/task/edit.blade.php
@@ -1,8 +1,8 @@
 <flux:modal name="panels.workspace.dashboard.task.edit.modal" class="md:w-96" flyout position="right">
     <div class="space-y-6">
         <div>
-            <flux:heading size="lg">{{ __('app.create_user') }}</flux:heading>
-            <flux:text class="mt-2">{{ __('app.create_user_description') }}</flux:text>
+            <flux:heading size="lg">{{ __('app.edit_task') }}</flux:heading>
+            <flux:text class="mt-2">{{ __('app.edit_task_description') }}</flux:text>
         </div>
         <!-- Modal body -->
         <form wire:submit="edit" class="space-y-6 max-w-2xl">
