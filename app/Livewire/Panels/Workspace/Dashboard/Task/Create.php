0a201b86678f4ef602328e471344ee25011baf8d
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-07 15:28:46 +0330

    Update

---
+++ app/Livewire/Panels/Workspace/Dashboard/Task/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Dashboard\Task;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.dashboard.task.create');
+    }
+}
