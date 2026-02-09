099e321d163baf0b342ce9b9cab5741357d4e06c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-09 07:39:48 +0330

    Add #[Layout] attribute to Livewire Index components in workspace review

--- app/Livewire/Panels/Workspace/Review/Task/Index.php
+++ app/Livewire/Panels/Workspace/Review/Task/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Panels\Workspace\Review\Task;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('layouts.panels.workspace')]
     public function render()
     {
         return view('livewire.panels.workspace.review.task.index');
--- app/Livewire/Panels/Workspace/Review/User/Index.php
+++ app/Livewire/Panels/Workspace/Review/User/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Panels\Workspace\Review\User;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('layouts.panels.workspace')]
     public function render()
     {
         return view('livewire.panels.workspace.review.user.index');
