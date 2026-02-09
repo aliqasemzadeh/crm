a44e6b8540782cdfa0d4aee0869ebdee70017b1a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-09 07:30:25 +0330

    Add Assigns Livewire component and blade view for task creation form

---
+++ app/Livewire/Panels/Workspace/Task/Assigns.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Task;
+
+use Livewire\Component;
+
+class Assigns extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.task.assigns');
+    }
+}
