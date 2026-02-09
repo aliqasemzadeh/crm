458fcec4f2d8c9ae4b57e19564ebfbf28f844739
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-09 08:11:55 +0330

    Add user task management components with Create, Edit, and Activity functionality, refine Index with search and pagination, and update routes for workspace review

--- app/Livewire/Panels/Workspace/Review/User/Index.php
+++ app/Livewire/Panels/Workspace/Review/User/Index.php
@@ -2,14 +2,36 @@
 
 namespace App\Livewire\Panels\Workspace\Review\User;
 
+use App\Models\User;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+
     #[Layout('layouts.panels.workspace')]
     public function render()
     {
-        return view('livewire.panels.workspace.review.user.index');
+        $users = User::query()
+            ->whereHas('tasks')
+            ->when($this->search, function ($query) {
+                $query->where(function ($q) {
+                    $q->where('first_name', 'like', '%' . $this->search . '%')
+                        ->orWhere('last_name', 'like', '%' . $this->search . '%')
+                        ->orWhere('email', 'like', '%' . $this->search . '%');
+                });
+            })
+            ->withCount(['tasks' => function ($query) {
+                $query->where('approval_status', '!=', 'approved');
+            }])
+            ->paginate(12);
+
+        return view('livewire.panels.workspace.review.user.index', [
+            'users' => $users
+        ]);
     }
 }
---
+++ app/Livewire/Panels/Workspace/Review/User/Task/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Review\User\Task;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.review.user.task.create');
+    }
+}
