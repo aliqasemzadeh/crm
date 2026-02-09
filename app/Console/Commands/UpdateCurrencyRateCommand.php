ce9a081a3a581dc464d98822ab234ae60524c382
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-09 11:49:35 +0330

    Add UpdateCurrencyRateCommand and bale configuration file for currency rate updates

---
+++ app/Console/Commands/UpdateCurrencyRateCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateCurrencyRateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-currency-rate-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ config/bale.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'bot_name' => 'SetareganCRMBot',
+    'bot_token' => '47361680:2XDfKTRdJ3wM1CiSfxgsIW4qDp8Ti2Pb-K8',
+    'bot_group_chat_id' => '4716603133'
+];
