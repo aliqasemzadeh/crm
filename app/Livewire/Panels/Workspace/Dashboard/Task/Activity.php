7a2c0c8d3872613f0741d229ef37f7099c1658d9
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-09 07:44:30 +0330

    Refactor Activity Livewire component and view by moving them to the Task directory for improved structure

---
+++ app/Livewire/Panels/Workspace/Dashboard/Task/Activity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Dashboard\Task;
+
+use Livewire\Component;
+
+class Activity extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.dashboard.task.activity');
+    }
+}
