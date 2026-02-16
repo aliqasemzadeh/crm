7b3b51d35dbeb5a3ff79d938d0f310e0d004a627
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-16 14:15:57 +0330

    Remove `position="left"` from announcement modals for consistent design adjustments

--- resources/views/livewire/panels/administrator/announcement/users.blade.php
+++ resources/views/livewire/panels/administrator/announcement/users.blade.php
@@ -1,4 +1,4 @@
-<flux:modal name="panels.administrator.announcement.users.modal" class="md:min-w-[600px]">
+<flux:modal name="panels.administrator.announcement.users.modal" flyout class="md:min-w-[600px]">
     <div class="space-y-6">
         <div>
             <flux:heading size="lg">{{ __('app.viewed_users') }}</flux:heading>
