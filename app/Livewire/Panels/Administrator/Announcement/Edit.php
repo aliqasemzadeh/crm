6d3d47f27ceaec1fe7662944928c970a41b20db7
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-16 13:23:42 +0330

    Add Livewire components and views for managing announcements, update Farsi translations

---
+++ app/Livewire/Panels/Administrator/Announcement/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Administrator\Announcement;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.administrator.announcement.edit');
+    }
+}
