2d11e9362ce347a99358016ec04892ce4e19ec77
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-16 10:55:32 +0330

    no error on update

--- app/Console/Commands/UpdateProjectCommand.php
+++ app/Console/Commands/UpdateProjectCommand.php
@@ -31,8 +31,6 @@ class UpdateProjectCommand extends Command
         $output = null;
         $retval = null;
         exec('git pull', $output, $retval);
-        echo "Returned with status $retval and output:\n";
-        print_r($output);
         Artisan::call("migrate");
     }
 }
