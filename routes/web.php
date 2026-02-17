329d923f45412a969b95c7adf72accaf1e20813c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-02-17 11:50:08 +0330

    Update signed route name for customer invoice view in SMS job and web routes

--- routes/web.php
+++ routes/web.php
@@ -59,5 +59,5 @@ Route::middleware(['auth'])->group( function () {
 });
 
 Route::get('/invoice/{invoiceId}/view', \App\Livewire\Panels\Customer\Invoice\View::class)
-    ->name('invoice.view')
+    ->name('panels.customer.invoice.view')
     ->middleware('signed');
