ba2a4cd3c3ca8d726f46e955230d441a03619a48
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-08 08:29:07 +0330

    update

--- app/Console/Commands/UpdateCurrencyRateCommand.php
+++ app/Console/Commands/UpdateCurrencyRateCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 
 use App\Models\CurrencyRate;
 use App\Jobs\Notification\BaleSendMessageJob;
+use App\Jobs\Notification\SendSmsMessageJob;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
 
@@ -95,6 +96,7 @@ class UpdateCurrencyRateCommand extends Command
             }
 
             BaleSendMessageJob::dispatch(trim($message));
+            SendSmsMessageJob::dispatch("09177114358", trim($message));
             $this->info(__('currencies.notification_dispatched'));
         } else {
             $this->info(__('currencies.outside_hours'));
