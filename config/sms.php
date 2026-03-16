c0f527890b548bfcd556e350cdd2de37625ab734
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-16 09:45:46 +0330

    price alert notification fix.

--- config/sms.php
+++ config/sms.php
@@ -2,5 +2,5 @@
 
 return [
     'api-key' => env('SMS_API_KEY', 'sa3102705417:PFqzIaS3CAw1ptAQ2nWkMczMHmk6ou49VfXd'),
-    'gateway' => env('SMS_GATEWAY', '1000884786'),
+    'gateway' => env('SMS_GATEWAY', '50003190'),
 ];
