1bf83556dec8bd32e8cf16e3044233d365ee99ff
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-11 12:42:17 +0330

    add Livewire components and views for managing workspace instructions

---
+++ app/Livewire/Panels/Workspace/Instruction/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Instruction;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.instruction.edit');
+    }
+}
---
+++ app/Livewire/Panels/Workspace/Instruction/File/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\Instruction\File;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.instruction.file.edit');
+    }
+}
