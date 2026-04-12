8623105331a07a2d9d4bc5e0c9f67569c0bef496
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-12 17:49:16 +0330

    add PurchaseRequest module with Livewire components, database migrations, model updates, and routing integration

---
+++ app/Livewire/Panels/Workspace/PurchaseRequest/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\PurchaseRequest;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.purchase-request.create');
+    }
+}
---
+++ app/Livewire/Panels/Workspace/PurchaseRequest/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panels\Workspace\PurchaseRequest;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.panels.workspace.purchase-request.index');
+    }
+}
