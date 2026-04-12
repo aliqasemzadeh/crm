b54dd6c332aed5f8c821bed711188df01ff90c64
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-12 17:36:14 +0330

    integrate approvable model functionality into PurchaseRequest, including traits and approval completion logic

--- app/Models/Workspace/PurchaseRequest.php
+++ app/Models/Workspace/PurchaseRequest.php
@@ -3,8 +3,15 @@
 namespace App\Models\Workspace;
 
 use Illuminate\Database\Eloquent\Model;
+use RingleSoft\LaravelProcessApproval\Contracts\ApprovableModel;
+use RingleSoft\LaravelProcessApproval\Models\ProcessApproval;
 
-class PurchaseRequest extends Model
+class PurchaseRequest extends Model implements ApprovableModel
 {
-    //
+    use \RingleSoft\LaravelProcessApproval\Traits\Approvable;
+
+    public function onApprovalCompleted(ProcessApproval $approval): bool
+    {
+        return true;
+    }
 }
