ac24e041f22a4bf458d3faca8a2112968fafb22f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-12 17:31:57 +0330

    remove workspace request and request type modules, including migrations, models, Livewire components, and related views

--- database/migrations/2026_04_12_135540_create_purchase_requests_table.php
+++ database/migrations/2026_04_12_135540_create_purchase_requests_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('workspace_requests', function (Blueprint $table) {
-            $table->integer('request_counter')->nullable()->after('request_number');
+        Schema::create('purchase_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('workspace_requests', function (Blueprint $table) {
-            $table->dropColumn('request_counter');
-        });
+        Schema::dropIfExists('purchase_requests');
     }
 };
