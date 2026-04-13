4ec125668856bf8ec8b3f6cfe9a8401abff2efb1
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-13 17:00:24 +0330

    refactor layouts and navigation components; enhance sidebar highlighting, improve grouping layout, and adjust table input styling

--- resources/views/partials/panels.blade.php
+++ resources/views/partials/panels.blade.php
@@ -1,23 +1,23 @@
 <flux:sidebar.nav>
     @can('user_access')
-        <flux:sidebar.item icon="user" href="{{ route('panels.user.dashboard.index') }}" wire:navigate>{{ __('app.user_panel') }}</flux:sidebar.item>
+        <flux:sidebar.item icon="user" href="{{ route('panels.user.dashboard.index') }}" :current="request()->routeIs('panels.user.*')" wire:navigate>{{ __('app.user_panel') }}</flux:sidebar.item>
     @endcan
 
         @can('workspace_access')
-            <flux:sidebar.item icon="columns-3-cog" href="{{ route('panels.workspace.dashboard.index') }}" wire:navigate>{{ __('app.workspace') }}</flux:sidebar.item>
+            <flux:sidebar.item icon="columns-3-cog" href="{{ route('panels.workspace.dashboard.index') }}" :current="request()->routeIs('panels.workspace.*')"  wire:navigate>{{ __('app.workspace') }}</flux:sidebar.item>
         @endcan
         @can('administrator_access')
-            <flux:sidebar.item icon="user-star" href="{{ route('panels.administrator.dashboard.index') }}" wire:navigate>{{ __('app.administrator') }}</flux:sidebar.item>
+            <flux:sidebar.item icon="user-star" href="{{ route('panels.administrator.dashboard.index') }}" :current="request()->routeIs('panels.administrator.*')" wire:navigate>{{ __('app.administrator') }}</flux:sidebar.item>
         @endcan
         @can('accounting_access')
-            <flux:sidebar.item icon="chart-no-axes-combined" href="{{ route('panels.accounting.dashboard.index') }}" wire:navigate>{{ __('app.accounting') }}</flux:sidebar.item>
+            <flux:sidebar.item icon="chart-no-axes-combined" href="{{ route('panels.accounting.dashboard.index') }}" :current="request()->routeIs('panels.accounting.*')" wire:navigate>{{ __('app.accounting') }}</flux:sidebar.item>
         @endcan
     @can('service_center_access')
-    <flux:sidebar.item icon="cpu" href="{{ route('panels.service-center.dashboard.index') }}" wire:navigate>{{ __('app.service_center') }}</flux:sidebar.item>
+    <flux:sidebar.item icon="cpu" href="{{ route('panels.service-center.dashboard.index') }}" :current="request()->routeIs('panels.service-center.*')" wire:navigate>{{ __('app.service_center') }}</flux:sidebar.item>
     @endcan
 
     @can('crm_access')
-    <flux:sidebar.item icon="handshake" href="{{ route('panels.crm.dashboard.index') }}" wire:navigate>{{ __('app.crm') }}</flux:sidebar.item>
+    <flux:sidebar.item icon="handshake" href="{{ route('panels.crm.dashboard.index') }}" :current="request()->routeIs('panels.crm.*')" wire:navigate>{{ __('app.crm') }}</flux:sidebar.item>
     @endcan
 
 
