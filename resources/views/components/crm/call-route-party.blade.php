8324aefbb81c8d148153b1bed9752288a12f8b43
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-05-03 19:51:36 +0330

    refactor UI components in User Edit form and CRM views for improved readability and maintenance

--- resources/views/components/crm/call-route-party.blade.php
+++ resources/views/components/crm/call-route-party.blade.php
@@ -2,15 +2,40 @@
     'party' => [],
 ])
 
-@if (! empty($party['is_two_digit_party']))
-    <span class="inline-flex max-w-full min-w-0 items-center gap-2">
-        @if (! empty($party['has_avatar']) && ! empty($party['avatar_url']))
-            <flux:avatar src="{{ $party['avatar_url'] }}" size="xs" />
-        @else
-            <flux:avatar name="{{ $party['avatar_name'] }}" color="auto" size="xs" />
-        @endif
-        <span dir="ltr" class="truncate font-medium text-zinc-800 dark:text-zinc-200">{{ $party['display'] }}</span>
-    </span>
+@php
+    $tooltipContent = isset($party['tooltip_number']) && $party['tooltip_number'] !== null && $party['tooltip_number'] !== ''
+        ? (string) $party['tooltip_number']
+        : null;
+@endphp
+
+@if (! empty($party['is_internal_user']))
+    @if ($tooltipContent !== null)
+        <flux:tooltip content="{{ $tooltipContent }}">
+            <span class="inline-flex max-w-full min-w-0 cursor-default items-center gap-2 outline-none" tabindex="0">
+                @if (! empty($party['avatar_url']))
+                    <flux:avatar src="{{ $party['avatar_url'] }}" size="xs" />
+                @else
+                    <flux:avatar name="{{ $party['avatar_name'] ?? $party['display'] }}" color="auto" size="xs" />
+                @endif
+                <span dir="ltr" class="truncate font-medium text-zinc-800 dark:text-zinc-200">{{ $party['display'] }}</span>
+            </span>
+        </flux:tooltip>
+    @else
+        <span class="inline-flex max-w-full min-w-0 items-center gap-2">
+            @if (! empty($party['avatar_url']))
+                <flux:avatar src="{{ $party['avatar_url'] }}" size="xs" />
+            @else
+                <flux:avatar name="{{ $party['avatar_name'] ?? $party['display'] }}" color="auto" size="xs" />
+            @endif
+            <span dir="ltr" class="truncate font-medium text-zinc-800 dark:text-zinc-200">{{ $party['display'] }}</span>
+        </span>
+    @endif
 @else
-    <span dir="ltr" class="font-medium text-zinc-800 dark:text-zinc-200">{{ $party['display'] }}</span>
+    @if ($tooltipContent !== null)
+        <flux:tooltip content="{{ $tooltipContent }}">
+            <span dir="ltr" class="cursor-default font-medium text-zinc-800 outline-none dark:text-zinc-200" tabindex="0">{{ $party['display'] }}</span>
+        </flux:tooltip>
+    @else
+        <span dir="ltr" class="font-medium text-zinc-800 dark:text-zinc-200">{{ $party['display'] }}</span>
+    @endif
 @endif
