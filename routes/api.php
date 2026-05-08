175c3e2f0833a668fcc2df85ad72398423d72b75
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-05-08 14:35:51 +0330

    Add logging functionality to ContactController and update routes

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\API\V1\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('contact/search/{number}', [ContactController::class, 'search']);
+Route::any('contact/log', [ContactController::class, 'log']);
