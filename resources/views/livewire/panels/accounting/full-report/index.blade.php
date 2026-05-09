3f442fc688c51461ed964374f543410e17939ece
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-05-09 20:17:40 +0330

    Enhance balance display in FullReport component by formatting negative values with parentheses for better clarity

--- resources/views/livewire/panels/accounting/full-report/index.blade.php
+++ resources/views/livewire/panels/accounting/full-report/index.blade.php
@@ -15,13 +15,31 @@
             <flux:text class="text-green-600 dark:text-green-400 font-bold">{{ __('app.credit') }}</flux:text>
             <flux:heading size="lg">{{ number_format($this->totals['credit']) }} {{ __('app.rial') }}</flux:heading>
         </flux:card>
+        @php
+            $totalsBalance = $this->totals['debit'] - $this->totals['credit'];
+            $totalsFinalBalance = ($this->totals['debit'] + $this->totals['uncashed_payments']) - ($this->totals['credit'] + $this->totals['uncashed_receipts']);
+        @endphp
         <flux:card class="flex flex-col items-center justify-center p-4 bg-blue-50 dark:bg-blue-950/20">
             <flux:text class="text-blue-600 dark:text-blue-400 font-bold">{{ __('app.balance') }}</flux:text>
-            <flux:heading size="lg">{{ number_format($this->totals['debit'] - $this->totals['credit']) }} {{ __('app.rial') }}</flux:heading>
+            <flux:heading size="lg">
+                @if($totalsBalance < 0)
+                    ({{ number_format(abs($totalsBalance)) }})
+                @else
+                    {{ number_format($totalsBalance) }}
+                @endif
+                {{ __('app.rial') }}
+            </flux:heading>
         </flux:card>
         <flux:card class="flex flex-col items-center justify-center p-4 bg-amber-50 dark:bg-amber-950/20">
             <flux:text class="text-amber-600 dark:text-amber-400 font-bold">{{ __('app.final_balance') }}</flux:text>
-            <flux:heading size="lg">{{ number_format(($this->totals['debit'] + $this->totals['uncashed_payments']) - ($this->totals['credit'] + $this->totals['uncashed_receipts'])) }} {{ __('app.rial') }}</flux:heading>
+            <flux:heading size="lg">
+                @if($totalsFinalBalance < 0)
+                    ({{ number_format(abs($totalsFinalBalance)) }})
+                @else
+                    {{ number_format($totalsFinalBalance) }}
+                @endif
+                {{ __('app.rial') }}
+            </flux:heading>
         </flux:card>
     </div>
 
@@ -50,12 +68,20 @@
                         <flux:table.cell>{{ number_format($party->debit) }}</flux:table.cell>
                         <flux:table.cell>{{ number_format($party->credit) }}</flux:table.cell>
                         <flux:table.cell class="{{ $balance > 0 ? 'text-red-500' : ($balance < 0 ? 'text-green-500' : '') }}">
-                            {{ number_format(abs($balance)) }} {{ $balance > 0 ? ')(' : ($balance < 0 ? '(بس)' : '') }}
+                            @if($balance < 0)
+                                ({{ number_format(abs($balance)) }})
+                            @else
+                                {{ number_format($balance) }}
+                            @endif
                         </flux:table.cell>
                         <flux:table.cell>{{ number_format($party->uncashed_receipts) }}</flux:table.cell>
                         <flux:table.cell>{{ number_format($party->uncashed_payments) }}</flux:table.cell>
                         <flux:table.cell class="font-bold {{ $finalBalance > 0 ? 'text-red-600' : ($finalBalance < 0 ? 'text-green-600' : '') }}">
-                            {{ number_format(abs($finalBalance)) }} {{ $finalBalance > 0 ? ')(' : ($finalBalance < 0 ? '(بس)' : '') }}
+                            @if($finalBalance < 0)
+                                ({{ number_format(abs($finalBalance)) }})
+                            @else
+                                {{ number_format($finalBalance) }}
+                            @endif
                         </flux:table.cell>
                     </flux:table.row>
                 @endforeach
