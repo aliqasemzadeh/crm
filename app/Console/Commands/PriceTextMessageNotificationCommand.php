c0b9aa8caa11b93582820b4101ce6fae0bc0db57
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-05-17 14:54:38 +0330

    Update PriceTextMessageNotificationCommand to include an additional phone number for notifications, enhancing communication reach.

--- app/Console/Commands/PriceTextMessageNotificationCommand.php
+++ app/Console/Commands/PriceTextMessageNotificationCommand.php
@@ -30,7 +30,7 @@ class PriceTextMessageNotificationCommand extends Command
     public function handle()
     {
         $phones = [
-            "09177886099", "09177114358"
+            "09177886099", "09177114358", "09372263131"
         ];
         $symbols = [
             'USDT' => [
