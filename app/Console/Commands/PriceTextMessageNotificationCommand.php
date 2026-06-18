3d17a00801b7acc71d73c22ff4361a39e2622256
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-06-18 16:41:11 +0330

    Add additional phone numbers to `PriceTextMessageNotificationCommand`

--- app/Console/Commands/PriceTextMessageNotificationCommand.php
+++ app/Console/Commands/PriceTextMessageNotificationCommand.php
@@ -32,7 +32,9 @@ class PriceTextMessageNotificationCommand extends Command
         $phones = [
             "09177886099",
              "09177114358",
-             "09372263131"
+             "09372263131",
+            "09303979067",
+            "09164966900"
 
         ];
         $symbols = [
