44530af1526098e3b21d94958ee73822f40517e9
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-08 10:15:28 +0330

    Remove unused `<x-date-select>` component, update and optimize `date-select` logic with `x-on` bindings, improve invoice button UX, and add missing `.env` defaults for Sepidar configuration

--- config/sepidar.php
+++ config/sepidar.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'FiscalYearRef' => env('FISCAL_YEAR_REF', 5),
+    'FiscalYearRef' => env('FISCAL_YEAR_REF', 3),
     'BaseRate' => 1280000,
     'Creator' => env('SEPIDAR_CREATOR', 1),
     'CurrencyRef' => env('SEPIDAR_CURRENCY_REF', 1),
-    'InvoiceSLRef' => env('SEPIDAR_INVOICE_SL_REF'),
+    'InvoiceSLRef' => env('SEPIDAR_INVOICE_SL_REF', 117),
     'InvoiceState' => env('SEPIDAR_INVOICE_STATE', 1),
     'SettlementType' => env('SEPIDAR_SETTLEMENT_TYPE', 1),
-    'DefaultStockRef' => env('SEPIDAR_DEFAULT_STOCK_REF'),
+    'DefaultStockRef' => env('SEPIDAR_DEFAULT_STOCK_REF', 2),
 ];
