09e59ff89ebc3d871ef1d78e9249ee405010a0ea
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-08 18:45:05 +0330

    Add initial commands, jobs, models, and Blade templates for cash-back discount codes
    - Introduced `CashBackDiscountCodeCommand` under `App\Console\Commands`.
    - Added `CashBackDiscountCodeJob` for handling discount code tasks asynchronously.
    - Created `DiscountCode` model to represent discount codes in the database.
    - Added new Blade components (`create`, `edit`, `delete`) for managing cash-back rules in CRM.

---
+++ app/Console/Commands/SetareganCo/CashBackDiscountCodeCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands\SetareganCo;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:cash-back-discount-code-command')]
+#[Description('Command description')]
+class CashBackDiscountCodeCommand extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
