53449ae00362f15f03214fbb918863ca82aa7a91
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-08 12:39:03 +0330

    Update item price management: add grouping functionality, Livewire components, improved layout, and dynamic data rendering in blade views.

--- resources/views/livewire/panels/sale/item-price/items.blade.php
+++ resources/views/livewire/panels/sale/item-price/items.blade.php
@@ -28,17 +28,29 @@
                                 <livewire:panels.sale.item-price.item-image :itemId="$item->ItemID" :key="'item-image-'.$item->ItemID" />
                             </flux:table.cell>
                             <flux:table.cell>
-                                @if($item->IranCode)
-                                    <flux:button size="xs" target="_blank" type="link" href="https://setaregan.co/Product/{{ $item->IranCode }}"  variant="filled" color="rose">{{ __('app.website') }}</flux:button>
-                                    <flux:button size="xs" variant="primary" wire:click="$dispatch('panels.sale.item-price.edit-site.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.website_edit') }}</flux:button>
-                                @else
-                                    <flux:button size="xs" variant="danger" wire:click="$dispatch('panels.sale.item-price.edit-site.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.irancode_not_set') }}</flux:button>
-                                @endif
-                                    @can('sales_item_fetchers')
-                                    <flux:button size="xs" variant="primary" color="yellow" wire:click="$dispatch('panels.sale.item-price.fetchers.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.get_price') }}</flux:button>
+                                <div class="flex flex-wrap gap-1">
+                                    @can('sales_item_image')
+                                        <flux:tooltip content="{{ __('app.warehouse_item_upload_image') }}">
+                                            <flux:button size="xs" variant="primary" color="violet" icon="image" icon:variant="outline" wire:click="$dispatch('panels.sale.item.upload-image.assign-data', { id: '{{ $item->ItemID }}' })" />
+                                        </flux:tooltip>
                                     @endcan
-                                        <flux:button size="xs" variant="primary" color="sky" wire:click="$dispatch('panels.accounting.grouping.item.invoice.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.invoices') }}</flux:button>
-                                <flux:button size="xs" variant="primary" color="green" wire:click="$dispatch('panels.accounting.grouping.item.receipt.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.receipts') }}</flux:button>
+                                    @if($item->IranCode)
+                                        <flux:button size="xs" target="_blank" type="link" href="https://setaregan.co/Product/{{ $item->IranCode }}" variant="filled" color="rose">{{ __('app.website') }}</flux:button>
+                                        @can('sales_item_site_edit')
+                                            <flux:button size="xs" variant="primary" wire:click="$dispatch('panels.sale.item-price.edit-site.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.website_edit') }}</flux:button>
+                                        @endcan
+                                    @else
+                                        @can('sales_item_site_edit')
+                                            <flux:button size="xs" variant="danger" wire:click="$dispatch('panels.sale.item-price.edit-site.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.irancode_not_set') }}</flux:button>
+                                        @endcan
+                                    @endif
+                                    @can('sales_item_fetchers')
+                                        <flux:button size="xs" variant="primary" color="yellow" wire:click="$dispatch('panels.sale.item-price.fetchers.assign-data', { id: '{{ $item->ItemID }}' })">{{ __('app.get_price') }}</flux:button>
+                                    @endcan
+                                    <flux:tooltip content="{{ __('app.item_details') }}">
+                                        <flux:button size="xs" variant="primary" color="teal" icon="eye" icon:variant="outline" href="{{ route('panels.sale.item.view', $item->ItemID) }}" wire:navigate />
+                                    </flux:tooltip>
+                                </div>
                             </flux:table.cell>
                             <flux:table.cell>
                                 {{ $item->Title }}
