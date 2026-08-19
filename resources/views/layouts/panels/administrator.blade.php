a3cc5bbc48468f95b999e54789670c507d419e9e
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-19 14:37:08 +0330

    Add HR Report link to Administrator sidebar and update Persian translations
    - Added HR Report link to the sidebar in Administrator layout.
    - Updated Persian localization files (`app.php` and `permissions.php`) with new HR-related terms.

--- resources/views/layouts/panels/administrator.blade.php
+++ resources/views/layouts/panels/administrator.blade.php
@@ -16,6 +16,7 @@
             <flux:sidebar.item href="{{ route('panels.administrator.user-management.role.index') }}" wire:navigate>{{ __('app.roles') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('panels.administrator.user-management.permission.index') }}" wire:navigate>{{ __('app.permissions') }}</flux:sidebar.item>
             <flux:sidebar.item href="{{ route('panels.administrator.user-management.device.index') }}" wire:navigate>{{ __('app.devices') }}</flux:sidebar.item>
+            <flux:sidebar.item href="{{ route('panels.administrator.user-management.hr.index') }}" wire:navigate>{{ __('app.hr_report') }}</flux:sidebar.item>
         </flux:sidebar.group>
         <flux:sidebar.item icon="megaphone" href="{{ route('panels.administrator.announcement.index') }}" wire:navigate>{{ __('app.announcements') }}</flux:sidebar.item>
         @can('administrator_calender_index')
